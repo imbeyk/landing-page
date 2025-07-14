d4bc99392418d1aad01e740ec3d2a5ceb239e4e0
Mahdieh/ahi  Beyk <icecoffeecofe@gmail.com>
2025-07-14 19:12:50 +0330

    Update and rename landing.js to script.js

--- landing.js
+++
@@ -1,6 +0,0 @@
-const togglemenu = document.getElementById('menu-toggle');
-const navList = document.querySelector('nav ul'); 
-
-togglemenu.addEventListener('click', () => {
-    navList.classList.toggle('show');
-});
